e8e225ea32fedd45c7cad0e0a30c5ef47eaf9c2f
kuropengin <kurokouchi7dream@yahoo.co.jp>
2021-10-05 03:54:11 +0000

    日付入力機能の修正

--- public/js/serch.js
+++ public/js/serch.js
@@ -1,6 +1,12 @@
 
-new Picker(document.querySelector('.js-full-picker'), {
+new Picker(document.querySelector('.picker-from'), {
     controls: true,
-    format: 'YYYY-MM-DD HH:mm:ss.SSS',
+    format: 'YYYY-MM-DD',
+    headers: true
+});
+
+new Picker(document.querySelector('.picker-to'), {
+    controls: true,
+    format: 'YYYY-MM-DD',
     headers: true
 });
