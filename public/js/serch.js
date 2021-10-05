b7fe1291273eec39c27368b64dbf33b20063b6ee
kuropengin <kurokouchi7dream@yahoo.co.jp>
2021-10-05 05:43:25 +0000

    検索機能の一時実装

--- public/js/serch.js
+++ public/js/serch.js
@@ -10,3 +10,22 @@ new Picker(document.querySelector('.picker-to'), {
     format: 'YYYY-MM-DD',
     headers: true
 });
+
+function search_api(){
+    const form = document.getElementById("search_area");
+
+    const fd = new FormData(form)
+
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', "https://ayameapidev.yukkuriikouze.com/search/complex?page=1&show=10", true)
+
+    xhr.onload = function () {
+        if(xhr.status == 200){
+            ResponseData = JSON.parse(xhr.response)
+            console.log(ResponseData)
+        }
+    }
+    xhr.send(fd)
+}
+
+document.getElementById("search-btn").addEventListener("click", search_api, false)
